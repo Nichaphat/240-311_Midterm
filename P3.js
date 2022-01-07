ef5433548978ebafc6fa2049c3022180930a3bb3
nichaphat <nichaphatkongsub@gmail.com>
2022-01-07 22:41:24 +0700

    first commit

---
+++ P3.js
@@ -0,0 +1,12 @@
+// โปรแกรมตรวจสอบว่าตัวเลขในตัวแปรเป็นจำนวนเต็มบวก เต็มลบ หรือศูนย์
+
+let n = 10;
+if (n > 0) {
+    console.log("n is positive");
+} else if (n < 0) {
+    console.log("n is negative");
+} else {
+    console.log("n is zero");
+}
+
+// Result: n is positive
