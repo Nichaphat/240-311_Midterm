93df7840ed9721811999ba3985299a4ac3183e91
nichaphat <nichaphatkongsub@gmail.com>
2022-01-11 15:02:12 +0700

    commit

--- P2.js
+++ P2.js
@@ -1,17 +1,26 @@
 // โปรแกรมคำนวณเกรดเฉลี่ย 
 
-let score = 80;
+let score = 84;
 
 if ((score >= 80) && (score <= 100)) {
     console.log("You grade is A");
 }
-else if ((score >= 70) && (score <= 79)) {
+else if ((score >= 75) && (score <= 79)) {
+    console.log("You grade is B+");
+}
+else if ((score >= 70) && (score <= 74)) {
     console.log("You grade is B");
 }
-else if ((score >= 60) && (score <= 69)) {
+else if ((score >= 65) && (score <= 69)) {
+    console.log("You grade is C+");
+}
+else if ((score >= 60) && (score <= 64)) {
     console.log("You grade is C");
 }
-else if ((score >= 50) && (score <= 59)) {
+else if ((score >= 55) && (score <= 59)) {
+    console.log("You grade is D+");
+}
+else if ((score >= 50) && (score <= 54)) {
     console.log("You grade is D");
 }
 else {
